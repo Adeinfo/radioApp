1c718a2a3ed72ab7d17378346af53768364276d1
adeinfo <adeinfo2015@gmail.com>
2019-12-09 14:00:49 +0100

    Hamburger navigation

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // The code might not be functional enough but it works
 const date = new Date ('December 4, 2019');
+
+
+function onClickMenu(){
+    document.getElementById('menu').classList.toggle('change');
+    document.getElementById('links').classList.toggle('change');
+}
