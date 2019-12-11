ed4a31c75a56058c22c229a3177250ae1b682e6a
adeinfo <adeinfo2015@gmail.com>
2019-12-11 14:01:56 +0100

    addition hambuger effect

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const date = new Date ('December 4, 2019');
 
 function onClickMenu(){
     document.getElementById('menu').classList.toggle('change');
-    document.getElementById('links').classList.toggle('change');
+    document.getElementById('nav').classList.toggle('change');
+    document.getElementById('menu-bg').classList.toggle('change-bg');
 }
